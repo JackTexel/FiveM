e18a1add653f6e4d7317ea35c46fdf00c35121f5
JackTexel <jasperjansen1996@gmail.com>
2022-10-01 14:05:58 +0200

    test
    test

---
+++ br-menu/__resource.lua
@@ -0,0 +1,22 @@
+
+client_script 'ClientModule.lua'
+server_script 'ServerModule.lua'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/*'
+}
+client_script 'config.lua'
+client_script 'client.lua'
+
+exports {
+	 'AddButton' , 'SetTitle'
+}
+
+
+client_script '@mellorwitwas/client/import.lua'
